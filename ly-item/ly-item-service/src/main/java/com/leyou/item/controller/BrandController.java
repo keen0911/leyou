ffdb5829cb1692750f678e3b4afd3a31ccf935bd
keen <2409306823@qq.com>
2022-01-26 16:29:12 +0800

    Initial commit

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,66 @@
+package com.leyou.item.controller;
+
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.impl.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("brand")
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandByPage(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "rows", defaultValue = "5") Integer rows,
+            @RequestParam(value = "sortBy", required = false) String sortBy,
+            @RequestParam(value = "desc", defaultValue = "false") Boolean desc,
+            @RequestParam(value = "key", required = false) String key) {
+        PageResult<Brand> result = this.brandService.queryBrandByPageAndSort(page,rows,sortBy,desc, key);
+        if (result == null || result.getItems().size() == 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 新增品牌
+     * @param brand
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<Void> saveBrand(Brand brand, @RequestParam("categories") List<Long> cids) {
+        this.brandService.saveBrand(brand, cids);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /**
+     * 根据分类查询品牌
+     * @param cid
+     * @return
+     */
+    @GetMapping("cid/{cid}")
+    public ResponseEntity<List<Brand>> queryBrandByCategory(@PathVariable("cid") Long cid) {
+        List<Brand> list = this.brandService.queryBrandByCategory(cid);
+        if(list == null){
+            new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(list);
+    }
+    @GetMapping("list")
+    public ResponseEntity<List<Brand>> queryBrandByIds(@RequestParam("ids") List<Long> ids){
+        List<Brand> list = this.brandService.queryBrandByBrandIds(ids);
+        if (list == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(list);
+    }
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/impl/GoodsService.java
@@ -0,0 +1,203 @@
+package com.leyou.item.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.bo.SpuBo;
+import com.leyou.item.mapper.*;
+import com.leyou.item.pojo.*;
+import com.leyou.item.service.CategoryService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GoodsService {
+
+    @Autowired
+    private SpuMapper spuMapper;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    @Autowired
+    private SpuDetailMapper spuDetailMapper;
+
+    @Autowired
+    private SkuMapper skuMapper;
+
+    @Autowired
+    private StockMapper stockMapper;
+
+
+    public PageResult<SpuBo> querySpuByPageAndSort(Integer page, Integer rows, Boolean saleable, String key) {
+        // 1、查询SPU
+        // 分页,最多允许查100条
+        PageHelper.startPage(page, Math.min(rows, 100));
+        // 创建查询条件
+        Example example = new Example(Spu.class);
+        Example.Criteria criteria = example.createCriteria();
+        // 是否过滤上下架
+        if (saleable != null) {
+            criteria.orEqualTo("saleable", saleable);
+        }
+        // 是否模糊查询
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("title", "%" + key + "%");
+        }
+        Page<Spu> pageInfo = (Page<Spu>) this.spuMapper.selectByExample(example);
+
+        List<SpuBo> list = pageInfo.getResult().stream().map(spu -> {
+            // 2、把spu变为 spuBo
+            SpuBo spuBo = new SpuBo();
+            // 属性拷贝
+            BeanUtils.copyProperties(spu, spuBo);
+
+            // 3、查询spu的商品分类名称,要查三级分类
+            List<String> names = this.categoryService.queryNameByIds(
+                    Arrays.asList(spu.getCid1(), spu.getCid2(), spu.getCid3()));
+            // 将分类名称拼接后存入
+            spuBo.setCname(StringUtils.join(names, "/"));
+
+            // 4、查询spu的品牌名称
+            Brand brand = this.brandMapper.selectByPrimaryKey(spu.getBrandId());
+            spuBo.setBname(brand.getName());
+            return spuBo;
+        }).collect(Collectors.toList());
+
+        return new PageResult<>(pageInfo.getTotal(), list);
+    }
+
+    @Transactional
+    public void save(SpuBo spu) {
+        // 保存spu
+        spu.setSaleable(true);
+        spu.setValid(true);
+        spu.setCreateTime(new Date());
+        spu.setLastUpdateTime(spu.getCreateTime());
+        this.spuMapper.insert(spu);
+        // 保存spu详情
+        spu.getSpuDetail().setSpuId(spu.getId());
+        this.spuDetailMapper.insert(spu.getSpuDetail());
+
+        // 保存sku和库存信息
+        saveSkuAndStock(spu.getSkus(), spu.getId());
+    }
+
+    private void saveSkuAndStock(List<Sku> skus, Long spuId) {
+        for (Sku sku : skus) {
+            if (!sku.getEnable()) {
+                continue;
+            }
+            // 保存sku
+            sku.setSpuId(spuId);
+            // 默认不参与任何促销
+            sku.setCreateTime(new Date());
+            sku.setLastUpdateTime(sku.getCreateTime());
+            this.skuMapper.insert(sku);
+
+            // 保存库存信息
+            Stock stock = new Stock();
+            stock.setSkuId(sku.getId());
+            stock.setStock(sku.getStock());
+            this.stockMapper.insert(stock);
+        }
+    }
+
+    public SpuDetail querySpuDetailById(Long id) {
+        return this.spuDetailMapper.selectByPrimaryKey(id);
+    }
+
+    public List<Sku> querySkuBySpuId(Long spuId) {
+        // 查询sku
+        Sku record = new Sku();
+        record.setSpuId(spuId);
+        List<Sku> skus = this.skuMapper.select(record);
+        for (Sku sku : skus) {
+            // 同时查询出库存
+            sku.setStock(this.stockMapper.selectByPrimaryKey(sku.getId()).getStock());
+        }
+        return skus;
+    }
+
+
+    public SpuBo queryGoodsById(Long id) {
+        /**
+         * 第一页所需信息如下：
+         * 1.商品的分类信息、所属品牌、商品标题、商品卖点（子标题）
+         * 2.商品的包装清单、售后服务
+         */
+        Spu spu=this.spuMapper.selectByPrimaryKey(id);
+        SpuDetail spuDetail = this.spuDetailMapper.selectByPrimaryKey(spu.getId());
+
+        Example example = new Example(Sku.class);
+        example.createCriteria().andEqualTo("spuId",spu.getId());
+        List<Sku> skuList = this.skuMapper.selectByExample(example);
+        List<Long> skuIdList = new ArrayList<>();
+        for (Sku sku : skuList){
+            skuIdList.add(sku.getId());
+        }
+
+        List<Stock> stocks = this.stockMapper.selectByIdList(skuIdList);
+
+        for (Sku sku:skuList){
+            for (Stock stock : stocks){
+                if (sku.getId().equals(stock.getSkuId())){
+                    sku.setStock(stock.getStock());
+                }
+            }
+        }
+
+        SpuBo spuBo = new SpuBo(spu.getBrandId(),spu.getCid1(),spu.getCid2(),spu.getCid3(),spu.getTitle(),
+                spu.getSubTitle(),spu.getSaleable(),spu.getValid(),spu.getCreateTime(),spu.getLastUpdateTime());
+        spuBo.setSpuDetail(spuDetail);
+        spuBo.setSkus(skuList);
+        return spuBo;
+    }
+
+    @Transactional
+    public void update(SpuBo spu) {
+        // 查询以前sku
+        List<Sku> skus = this.querySkuBySpuId(spu.getId());
+        // 如果以前存在，则删除
+        if(!CollectionUtils.isEmpty(skus)) {
+            List<Long> ids = skus.stream().map(s -> s.getId()).collect(Collectors.toList());
+            // 删除以前库存
+            Example example = new Example(Stock.class);
+            example.createCriteria().andIn("skuId", ids);
+            this.stockMapper.deleteByExample(example);
+
+            // 删除以前的sku
+            Sku record = new Sku();
+            record.setSpuId(spu.getId());
+            this.skuMapper.delete(record);
+
+        }
+        // 新增sku和库存
+        saveSkuAndStock(spu.getSkus(), spu.getId());
+
+        // 更新spu
+        spu.setLastUpdateTime(new Date());
+        spu.setCreateTime(null);
+        spu.setValid(null);
+        spu.setSaleable(null);
+        this.spuMapper.updateByPrimaryKeySelective(spu);
+
+        // 更新spu详情
+        this.spuDetailMapper.updateByPrimaryKeySelective(spu.getSpuDetail());
+    }
+
+}
